4330711c4271bf40f1a9ab567c1663b7d9e07c1d
Ayoub Bousakri <ayoub123bousakri@gmail.com>
2023-01-27 17:19:52 +0100

    Manage User Done

--- routes/web.php
+++ routes/web.php
@@ -28,6 +28,16 @@ Route::get('/admin/logout', [AdminController::class , 'Logout'])->name('admin.lo
 // User Management All Routes
 Route::prefix('users')->group(function(){
 
-     Route::get('/view' , [UserController::class , 'UserView'])->name('user.view');
+    Route::get('/view' , [UserController::class , 'UserView'])->name('user.view');
 
- });
+    Route::get('/add' , [UserController::class , 'UserAdd'])->name('users.add');
+   
+    Route::post('/store' , [UserController::class , 'UserStore'])->name('users.store');
+    
+    Route::get('/edit/{id}' , [UserController::class , 'UserEdit'])->name('users.edit');
+    
+    Route::post('/update/{id}' , [UserController::class , 'UserUpdate'])->name('users.update');
+
+    Route::get('/delete/{id}' , [UserController::class , 'UserDelete'])->name('users.delete');
+
+});
